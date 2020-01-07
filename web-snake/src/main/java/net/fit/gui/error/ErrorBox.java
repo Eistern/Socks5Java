3a9b02c7e3fc24a1864e793769156c40565efa60
Eistern <zulind@inbox.ru>
2020-01-07 17:08:20 +0700

    Removed web-snake

--- web-snake/src/main/java/net/fit/gui/error/ErrorBox.java
+++
@@ -1,9 +0,0 @@
-package net.fit.gui.error;
-
-import javax.swing.*;
-
-public class ErrorBox {
-    public static void showError(String content) {
-        JOptionPane.showMessageDialog(new JFrame(), content, "Error", JOptionPane.ERROR_MESSAGE);
-    }
-}
